e746eb531d1077a14d61228a9d0353c898d55bfd
Ynã Ponte <ynaqp@alu.ufc.br>
2025-08-14 10:32:06 -0300

    change: inclusão da nova feature, o decorador cache_execution

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,4 @@
-from .database_manager import VectorDatabaseManager
+from src.utils.database_manager import VectorDatabaseManager
+from src.utils.cache_results_decorator import cache_execution
+
+__all__ = ["VectorDatabaseManager", "cache_execution"]
